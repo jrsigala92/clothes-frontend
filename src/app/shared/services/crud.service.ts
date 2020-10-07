b2f23c8f24344f3708cb60ba11823d80e60cbc8d
jrsigala92 <jsigala92@outlook.com>
2020-10-06 21:00:14 -0700

    endpoint changed

--- src/app/shared/services/crud.service.ts
+++ src/app/shared/services/crud.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  protected apiUrl = 'http://localhost:3000/';
+  protected apiUrl = environment.apiUrl;
   protected endpoint: string;
   constructor(protected httpService: HttpService) { }
 
