22a5ef52da5cb8a392eedf89b7969cee25f73b38
jrsigala92 <jsigala92@outlook.com>
2020-10-15 19:10:04 -0700

    updating variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://charomx.herokuapp.com/'
-  apiUrl: 'http://localhost:3000/'
+  apiUrl: 'https://charomx.herokuapp.com/'
+  // apiUrl: 'http://localhost:3000/'
 };
 
 /*
